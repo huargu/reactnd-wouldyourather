6544ccb126febe45396d8360586ea2ad96fe815a
Hüseyin Armağan Güler <h.armagan.g@gmail.com>
2021-12-28 23:49:30 +0300

    minor fix

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,18 +1,15 @@
 import { getInitialData } from "../util/api";
 import { getUsers } from "./users";
 import { getQuestions } from "./questions";
-import { showLoading, hideLoading } from "react-redux-loading";
 import { setAuthedUser } from "./authedUser";
 
 const INITIAL_AUTH_ID = "";
 export function handleInitialData() {
   return (dispatch) => {
-    dispatch(showLoading());
     return getInitialData().then(({ users, questions }) => {
       dispatch(getUsers(users));
       dispatch(getQuestions(questions));
       dispatch(setAuthedUser(INITIAL_AUTH_ID));
-      dispatch(hideLoading());
     });
   };
 }
